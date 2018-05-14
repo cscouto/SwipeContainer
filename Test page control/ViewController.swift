fc6620f3f647108627f5c96e156fce4e2ff48d52
Tiago Do Couto <cs.couto@outlook.com>
2018-05-14 18:04:59 -0500

    Initial Commit

---
+++ Test page control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test page control
+//
+//  Created by Tiago Do Couto on 5/14/18.
+//  Copyright © 2018 coutocode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
